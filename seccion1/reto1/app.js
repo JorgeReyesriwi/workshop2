e4ef470d698ed41652fa2978775c029a7fb4ce25
daguttt <dagutmu667@gmail.com>
2024-04-16 17:41:41 -0500

    Add seccion one

---
+++ seccion1/reto1/app.js
@@ -0,0 +1,32 @@
+// Julian's Trip
+
+const destination = window.prompt("Enter the destination you want to go to:");
+console.log(`Destination: ${destination}`);
+
+let daysCount = parseInt(window.prompt("Enter the number of days for the trip:"));
+console.log(`Number of days: ${daysCount}`);
+
+let budget = parseInt(window.prompt("Enter the estimated budget for the trip:"));
+console.log(`Budget: ${budget}`);
+
+const dailyCost = 80000;
+
+const totalCost = daysCount * dailyCost;
+
+if (budget >= totalCost) {
+  console.log("The budget is adequate for the trip.");
+} else {
+  console.log("The budget is not enough for the number of days.");
+
+  const possibleDays = budget / dailyCost; 
+
+  if (possibleDays === 0) {
+    console.log("With this budget, Julian cannot travel any day.");
+  } else {
+    console.log(`Julian can travel up to ${possibleDays} days with his current budget.`);
+  }
+
+  if (budget < totalCost){
+    console.log("To meet the travel cost, Julian should adjust his budget or the number of days to fit his initial budget.")
+  }
+}
---
+++ seccion1/reto2/app.js
@@ -0,0 +1,44 @@
+// Add a new souvenir
+function addSouvenir(souvenirList, name, cost, available) {
+  souvenirList.push({name, cost, available});
+  console.log(`"${name}" added to the list.`);
+}
+
+let souvenirList = [];
+
+function addSouvenir(souvenirList, name, cost, available) {
+  if (checkDataTypes(name, cost, available)) {
+      souvenirList.push({ name, cost, available });
+      console.log(`Souvenir "${name}" added to the list.`);
+  } else {
+      console.log("The souvenir could not be added because the entered data is not correct")
+  }
+}
+
+function enterSouvenir(numSouvenirs) {
+  if (numSouvenirs > 0) {
+      let name = window.prompt(`Enter the name of souvenir ${numSouvenirs}:`);
+      let cost = parseFloat(window.prompt(`Enter the cost of souvenir ${numSouvenirs}:`));
+      let available = window.confirm(`Is souvenir ${numSouvenirs} available? (Click OK for yes, Cancel for no)`);
+
+      addSouvenir(souvenirList, name, cost, available);
+      
+      enterSouvenir(numSouvenirs - 1); 
+  }
+}
+
+// Data types verification
+function checkDataTypes(name, cost, available) {
+  if (typeof name === 'string' && typeof cost === 'number' && typeof available === 'boolean') {
+      console.log("Data types are as expected")
+      return true;
+  } else {
+      console.log("Data types are not as expected")
+      return false;
+  }
+}
+
+let numSouvenirs = parseInt(window.prompt("Enter the number of souvenirs you want to enter:"));
+enterSouvenir(numSouvenirs);
+
+console.log(souvenirList);
